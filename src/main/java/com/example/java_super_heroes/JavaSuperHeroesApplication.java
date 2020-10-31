cb47b4d254926eac8fb4fdfa55c4fddc8b57afff
Carina Tiemi Yoshida <ctyoshida@gmail.com>
2020-10-31 14:29:44 -0400

    Estrutura base do projeto

---
+++ src/main/java/com/example/java_super_heroes/JavaSuperHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_super_heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSuperHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSuperHeroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_super_heroes/JavaSuperHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_super_heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSuperHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
